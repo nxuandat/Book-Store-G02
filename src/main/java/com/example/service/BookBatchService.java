f0e2ce5bc34d6db1acac631082a7f09515862157
nguyenhung <nguyen.vu.hung309@gmail.com>
2024-03-11 18:55:22 +0700

    Create Book Controller, Book Batch Controller, Main Class UI

---
+++ src/main/java/com/example/service/BookBatchService.java
@@ -0,0 +1,23 @@
+package com.example.service;
+
+import com.example.dao.BookBatchDao;
+import com.example.model.BookBatch;
+
+import java.util.List;
+
+public class BookBatchService {
+
+    private final BookBatchDao bookBatchDao;
+
+    public BookBatchService() {
+        this.bookBatchDao = new BookBatchDao();
+    }
+
+    // Lấy danh sách lô sách theo ID sách
+    public List<BookBatch> getBatchesByBookId(int bookId) {
+        return bookBatchDao.getBatchesByBookId(bookId);
+    }
+
+    // TODO: Thêm các phương thức khác cần thiết cho quản lý lô sách
+
+}
