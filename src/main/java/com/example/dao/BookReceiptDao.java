79d6cabaf971ec31affa2f1ffc83791c41d75f5c
huy194 <57141767+huy194@users.noreply.github.com>
2024-03-15 16:43:25 +0700

    Fixing error

--- src/main/java/com/example/dao/BookReceiptDao.java
+++ src/main/java/com/example/dao/BookReceiptDao.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.sql.Date;
+import java.sql.Statement;
 
 import main.java.com.example.model.BookBatch;
 import main.java.com.example.model.BookReceipt;
