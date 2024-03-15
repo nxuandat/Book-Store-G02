c7498f72922435e2fc3a64ea03061eb9f4a400d3
TonDucQuy <136158521+TonDucQuy@users.noreply.github.com>
2024-03-15 14:46:19 +0700

    PublisherDao/Service
    Add class Dao/Service for Publisher

--- src/main/java/com/example/dao/AuthorDao.java
+++ src/main/java/com/example/dao/AuthorDao.java
@@ -8,7 +8,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Date;
+// import java.util.Date;
 import java.util.List;
 
 public class AuthorDao {
